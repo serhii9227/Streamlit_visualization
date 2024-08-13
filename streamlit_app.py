0208db6efa54fddac08ec1e43edab1d09a587948
serhii9227 <160024560+serhii9227@users.noreply.github.com>
2024-08-13 23:06:15 +0000

    datafarme

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("🎈 EO statistics")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+st.subheader("Data Frame")
+
+df = pd.DataFrame(
+   np.random.randn(50, 20),
+   columns=('col %d' % i for i in range(20)))
+
+st.dataframe(df)  # Same as st.write(df)
